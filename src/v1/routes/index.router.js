d61adb1d86295ada01c4dbd63467db28637abe97
james <vungochung1207@gmail.com>
2023-12-17 22:58:06 +0700

    feature: add api user

--- src/v1/routes/index.router.js
+++ src/v1/routes/index.router.js
@@ -8,4 +8,25 @@ router.get('/checkstatus', (req, res, next) => {
     })
 })
 
+router.get('/api/users', (req, res, next) => {
+    res.status(200).json({
+        status: 'success api',
+        message: 'api ok',
+        metadata: [
+            {
+                name: 'James',
+                age: 28
+            },
+            {
+                name: 'James 1',
+                age: 29
+            },
+            {
+                name: 'James 2',
+                age: 30
+            }
+        ]
+    })
+})
+
 module.exports = router;
